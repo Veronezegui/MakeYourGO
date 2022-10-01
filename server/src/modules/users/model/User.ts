9155e00dde471aa754208cd5498350efda37c8fa
lucasabdouni <lucasabdouni@hotmail.com>
2022-10-01 18:01:29 -0300

    adicionado recovery password por email

--- server/src/modules/users/model/User.ts
+++ server/src/modules/users/model/User.ts
@@ -1,18 +1,20 @@
 /* eslint-disable no-unused-vars */
-import { v4 as uuidV4 } from 'uuid'
+import { v4 as uuidV4 } from "uuid";
 
 class User {
-  id?: string
-  name: string
-  email: string
-  senha: string
-  created_at: Date
+  id?: string;
+  name: string;
+  email: string;
+  senha: string;
+  passwordResetToken?: number;
+  passwordResetExpires?: Date;
+  created_at: Date;
 
   constructor() {
     if (!this.id) {
-      this.id = uuidV4()
+      this.id = uuidV4();
     }
   }
 }
 
-export { User }
+export { User };
