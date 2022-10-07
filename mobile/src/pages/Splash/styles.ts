488423599040c8efcb99cc39e042473e7303d088
lucasabdouni <lucasabdouni@hotmail.com>
2022-10-06 22:23:00 -0300

    add animation on splash

---
+++ mobile/src/pages/Splash/styles.ts
@@ -0,0 +1,12 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #0f172a;
+`;
+
+export const LogoView = styled.View``;
