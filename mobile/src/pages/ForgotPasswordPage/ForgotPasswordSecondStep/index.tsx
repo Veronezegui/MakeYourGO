6ba0d2d1fa74165d583606f818e48290d2604102
Veronezegui <veronezegui@gmail.com>
2022-10-07 19:38:52 -0300

    teste feito e pequenos ajustes

--- mobile/src/pages/ForgotPasswordPage/ForgotPasswordSecondStep/index.tsx
+++ mobile/src/pages/ForgotPasswordPage/ForgotPasswordSecondStep/index.tsx
@@ -1,24 +1,23 @@
 /* eslint-disable space-before-function-paren */
-import React, { useState } from "react";
-import { useNavigation, useRoute } from "@react-navigation/native";
-import { Container, Form, Logo, Subtitle, Title, TitleDiv } from "./styles";
-import Makeyourgologo from "../../../assets/makeyourgo.svg";
-import { Input } from "../../../components/Input";
-import { Button } from "../../../components/Button";
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, Alert, StatusBar } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { Container, Form, Logo, Subtitle, Title, TitleDiv } from './styles';
+import Makeyourgologo from '../../../assets/makeyourgo.svg';
+import { Input } from '../../../components/Input';
+import { Button } from '../../../components/Button';
 
-import { api } from "../../../services/api";
-import * as Yup from "yup";
-
-import { Alert, StatusBar } from "react-native";
+import { api } from '../../../services/api';
+import * as Yup from 'yup';
 
 interface Params {
   data: { email: string };
 }
 
 export function ForgotPasswordSecondStep() {
-  const [token, setToken] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [token, setToken] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
   const [isDisabled, setIsDisabled] = useState(false);
 
   const navigation = useNavigation();
@@ -32,12 +31,12 @@ export function ForgotPasswordSecondStep() {
     try {
       const schema = Yup.object().shape({
         email: Yup.string()
-          .email("E-mail inválido")
-          .required("E-mail é obrigatório"),
-        password: Yup.string().required("Senha é obrigatório"),
+          .email('E-mail inválido')
+          .required('E-mail é obrigatório'),
+        password: Yup.string().required('Senha é obrigatório'),
         passwordConfirm: Yup.string().required(
-          "Confirmação de Senha é obrigatório"
-        ),
+          'Confirmação de Senha é obrigatório'
+        )
       });
 
       const { email } = data;
@@ -48,37 +47,37 @@ export function ForgotPasswordSecondStep() {
         email,
         tokenNumber,
         password,
-        passwordConfirm,
+        passwordConfirm
       };
 
       await schema.validate(newData);
 
       if (password !== passwordConfirm) {
         setIsDisabled(false);
-        return Alert.alert("Opa, as senhas nao conferem !");
+        return Alert.alert('Opa, as senhas nao conferem !');
       }
 
       await api
-        .post("/users/reset_password", {
+        .post('/users/reset_password', {
           email,
           token: tokenNumber,
-          senha: password,
+          senha: password
         })
         .then(() => {
           setIsDisabled(false);
-          navigation.navigate("Confirmation", {
-            nextScreenRoute: "SignInPage",
-            title: "Resenha Alterada !",
-            message: `Agora é só fazer login\ne aproveitar`,
+          navigation.navigate('Confirmation', {
+            nextScreenRoute: 'SignInPage',
+            title: 'Senha Alterada !',
+            message: 'Agora é só fazer login\ne aproveitar'
           });
         });
     } catch (error) {
       if (error instanceof Yup.ValidationError) {
         setIsDisabled(false);
-        return Alert.alert("Opa", error.message);
+        return Alert.alert('Opa', error.message);
       } else {
         setIsDisabled(false);
-        return Alert.alert("Opa, o token informado é invalido ou expirado !");
+        return Alert.alert('Opa, o token informado é invalido ou expirado !');
       }
     }
   }
@@ -98,7 +97,7 @@ export function ForgotPasswordSecondStep() {
         <Title>Token enviado</Title>
 
         <Subtitle>
-          Foi enviado um token de validação {`\n`}para seu email.
+          Foi enviado um token de validação {'\n'}para seu email.
         </Subtitle>
       </TitleDiv>
       <Form>
